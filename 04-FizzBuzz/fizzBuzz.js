0365eed1624da85a93a85bd1cc4bd3ccf083bbdf
yasaminSalimi81 <yasisl2361@gmail.com>
2024-09-11 23:45:44 +0330

    Update fizzBuzz.js

--- 04-FizzBuzz/fizzBuzz.js
+++ 04-FizzBuzz/fizzBuzz.js
@@ -1,1 +1,9 @@
 // write your func here
+function fizzbuzz () {
+    for ( i = 1 ; i < 101 ; i++){
+        if ( i % 15 == 0 ) console.log("fizzbuzz");
+        else if ( i % 5 == 0 ) console.log("buzz");
+        else if ( i % 3 == 0 ) console.log("fizz");
+        else console.log(i);
+};
+};
