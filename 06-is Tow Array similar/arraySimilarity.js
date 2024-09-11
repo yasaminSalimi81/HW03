cad8fa366e3621d6317de58b8d89b887ad7d152c
yasaminSalimi81 <yasisl2361@gmail.com>
2024-09-11 23:46:32 +0330

    Update arraySimilarity.js

--- 06-is Tow Array similar/arraySimilarity.js
+++ 06-is Tow Array similar/arraySimilarity.js
@@ -1,1 +1,14 @@
 // write your func here
+function equality (array1,array2) {
+    let truthy = [];
+    if(array1.length == array2.length) {
+        for( item of array1) {
+            truthy[item] = array1[item] === array2[item] ; 
+        };
+        return truthy.every(function(boulean){
+            return boulean === true;
+        });
+    } else return false;
+}
+const results = equality([1,2,3,4,5,6,7,8,88] , [1,2,3,4,5,6,7,8,9]);
+console.log(results);
