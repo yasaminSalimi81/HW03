95f39057681886b1dd51754f140f88637618fea3
yasaminSalimi81 <yasisl2361@gmail.com>
2024-09-11 23:45:16 +0330

    Update fac.js

--- 03-factoriel/fac.js
+++ 03-factoriel/fac.js
@@ -1,1 +1,10 @@
 // write your func here
+function fact(number) {
+    let factoriel = 1 ;
+    for ( i = 1 ; i <= number ; i++) {
+        factoriel = factoriel * i ;
+    }
+    return factoriel;
+}
+const num = fact(3);
+console.log(num);
