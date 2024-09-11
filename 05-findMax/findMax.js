2c9f2a8916acc49df960d086d0d4f6263b0ebcdc
yasaminSalimi81 <yasisl2361@gmail.com>
2024-09-11 23:46:07 +0330

    Update findMax.js

--- 05-findMax/findMax.js
+++ 05-findMax/findMax.js
@@ -1,1 +1,12 @@
 // write your func here
+function arrays(arr) {
+    let bignumber = 0 ;
+    for ( i = 0 ; i < arr.length ; i ++) {
+        if (  bignumber < arr[i] ) {
+            bignumber = arr[i];
+        }
+    }
+    return bignumber;
+}
+const arr = arrays([2,5,1,15,7])
+console.log(arr);
