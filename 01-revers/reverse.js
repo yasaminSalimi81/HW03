91ceb995b7b99fa118725ca043277778d85b2cb3
yasaminSalimi81 <yasisl2361@gmail.com>
2024-09-11 23:44:11 +0330

    Update reverse.js

--- 01-revers/reverse.js
+++ 01-revers/reverse.js
@@ -1,1 +1,6 @@
 // write your func here
+function reverse (sentences) {
+    return sentences.split("").reverse().join("");
+}
+const sentences = reverse('i am yasamin salimi');
+console.log(sentences);
