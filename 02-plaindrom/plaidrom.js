e6217606d9abe64a2f503a238eed88fafe13d0d6
yasaminSalimi81 <yasisl2361@gmail.com>
2024-09-11 23:44:48 +0330

    Update plaidrom.js

--- 02-plaindrom/plaidrom.js
+++ 02-plaindrom/plaidrom.js
@@ -1,1 +1,8 @@
 // write your func here
+function pali(word) {
+    if ((word.split("").reverse().join("")) == word) {
+        return true;
+    } else return false;
+}
+const words = pali('racecar');
+console.log(words);
